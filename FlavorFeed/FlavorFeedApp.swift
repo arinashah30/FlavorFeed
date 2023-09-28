4f1898c0f44c535b4307e95cf2e669214ad9e757
arinashah30 <ashah634@gatech.edu>
2023-09-28 13:38:57 -0400

    Initial Commit

---
+++ FlavorFeed/FlavorFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFeedApp.swift
+//  FlavorFeed
+//
+//  Created by Arina Shah on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
