89c59191ec8576df90e80ebef895d8b91e46b415
Pawan Yerramilli <pawan.yerramilli@gmail.com>
2023-10-19 16:12:36 -0700

    Fix persistance

--- FlavorFeed/View Models/ViewModel.swift
+++ FlavorFeed/View Models/ViewModel.swift
@@ -11,17 +11,25 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class ViewModel: ObservableObject {
-    @Published private var current_user: User? = nil
+    @Published var current_user: User? = nil
     @Published var errorText: String? = nil
     
     let db = Firestore.firestore()
     let auth = Auth.auth()
     
+    init() {
+        let handle = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            if let user = user {
+                self?.setCurrentUser(userId: user.uid)
+            } else {
+                UserDefaults.standard.setValue(false, forKey: "log_Status")
+            }
+        }
+    }
+    
     func firebase_sign_out() {
         do {
-            try auth.signOut()
-            UserDefaults.standard.set(false, forKey: "log_Status")
-            
+            try auth.signOut()            
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
@@ -35,48 +43,6 @@ class ViewModel: ObservableObject {
                 self?.errorText = error.localizedDescription
                 return
             }
-            
-            if let user = authResult?.user {
-                let id = user.uid
-                self?.db.collection("USERS").document(id).setData(
-                    ["name" : displayName,
-                     "username" : username,
-                     "profilePicture" : "",
-                     "email" : email,
-                     "favorites" : [],
-                     "friends" : [],
-                     "savedPosts" : [],
-                     "bio" : "",
-                     "myPosts" : [],
-                     "phone_number" : phoneNumber,
-                     "location" : "",
-                     "myRecipes" : []
-                    ] as [String : Any]
-                ) { err in
-                    if let err = err {
-                        self?.errorText = err.localizedDescription
-                    } else {
-                        self?.current_user = User(
-                            id: id,
-                            name: displayName,
-                            username: username,
-                            profilePicture: "",
-                            email: email,
-                            favorites: [],
-                            friends: [],
-                            savedPosts: [],
-                            bio: "",
-                            myPosts: [],
-                            phoneNumber: Int(phoneNumber)!,
-                            location: "",
-                            myRecipes: [])
-                        
-                        UserDefaults.standard.setValue(true, forKey: "log_Status")
-                    }
-                }
-                
-            }
-            
         }
     }
     
@@ -84,30 +50,30 @@ class ViewModel: ObservableObject {
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
             if let error = error {
                 self?.errorText = error.localizedDescription
-            } else {
-                if let userId = authResult?.user.uid {
-                    self?.db.collection("USERS").document(userId).getDocument (completion: { document, error in
-                        if let error = error {
-                            self?.errorText = error.localizedDescription
-                        } else if let document = document {
-                            self?.current_user = User(id: userId,
-                                                      name: document["name"] as! String,
-                                                      username: document["username"] as! String,
-                                                      profilePicture: document["profilePicture"] as! String,
-                                                      email: document["email"] as! String,
-                                                      favorites: document["favorites"] as! [Post],
-                                                      friends: document["friends"] as! [User],
-                                                      savedPosts: document["savedPosts"] as! [Post],
-                                                      bio: document["bio"] as! String, 
-                                                      myPosts: document["myPosts"] as! [Post],
-                                                      phoneNumber: Int(document["phone_number"] as! String)!,
-                                                      location: document["location"] as! String,
-                                                      myRecipes: document["myRecipes"] as! [String])
-                            UserDefaults.standard.setValue(true, forKey: "log_Status")
-                        }
-                    })
-                }
             }
         }
+    }
+    
+    func setCurrentUser(userId: String) {
+        db.collection("USERS").document(userId).getDocument (completion: { [weak self] document, error in
+            if let error = error {
+                self?.errorText = error.localizedDescription
+            } else if let document = document {
+                self?.current_user = User(id: userId,
+                                          name: document["name"] as! String,
+                                          username: document["username"] as! String,
+                                          profilePicture: document["profilePicture"] as! String,
+                                          email: document["email"] as! String,
+                                          favorites: document["favorites"] as! [Post],
+                                          friends: document["friends"] as! [User],
+                                          savedPosts: document["savedPosts"] as! [Post],
+                                          bio: document["bio"] as! String,
+                                          myPosts: document["myPosts"] as! [Post],
+                                          phoneNumber: Int(document["phone_number"] as! String)!,
+                                          location: document["location"] as! String,
+                                          myRecipes: document["myRecipes"] as! [String])
+                UserDefaults.standard.setValue(true, forKey: "log_Status")
+            }
+        })
     }
 }
