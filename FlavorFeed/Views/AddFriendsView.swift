0c512057547785e7a553b4a41c6f816aa6d4b3f1
misry <dmisry.7.21.04@gmail.com>
2023-10-24 18:07:18 -0400

    search friends view

--- FlavorFeed/Views/AddFriendsView.swift
+++ FlavorFeed/Views/AddFriendsView.swift
@@ -2,7 +2,7 @@
 //  AddFriendsView.swift
 //  FlavorFeed
 //
-//  Created by Misry Dhanani, Husna Jakeer, and Triem Le on 10/12/23.
+//  Created by Misry Dhanani, Husna Jakeer, Subha Udhyakumar, and Triem Le on 10/12/23.
 //
 
 import SwiftUI
@@ -14,30 +14,48 @@ struct Person {
 struct AddFriendsView: View {
     @State private var selectedOption = "Suggestions"
     var options = ["Suggestions", "Friends", "Requests"]
-    @State private var searchText = "hello"
+    @State private var searchText = "Add or search friends"
     
     var body: some View {
-        VStack{
-            NavigationStack {
-                Text("Searching for \(searchText)")
-                    .navigationTitle("Searchable Example")
-            }
-            .searchable(text: $searchText)
-            let user = User(id: UUID(), name: "Triem", username: "triem123", password: "", profilePicture: "", email: "", favorites: [], friends: [], savedPosts: [], bio: "", myPosts: [], phoneNumber: 0, location: "", myRecipes: [])
-            UserListView(user: user)
-            NavigationView{
-                VStack{
-                    Text("\(selectedOption)")
-                    Picker("Tabs", selection: $selectedOption) {
-                        Text(options[0]).tag(options[0])
-                        Text(options[1]).tag(options[1])
-                        Text(options[2]).tag(options[2])
-                    }
-                    .pickerStyle(.segmented)
-                    .padding(.horizontal)
-                    .menuStyle(.borderlessButton)
+        ZStack {
+            VStack{
+                Text("Flavor Feed")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+                    .padding(.bottom, -10)
+                NavigationStack {
+                    //Text("\(searchText)")
                 }
-            } 
+                .searchable(text: $searchText)
+                .background(Color.ffTertiary)
+                Button(action: {}, label: {
+                    HStack{
+                        Image(systemName: "person.circle")
+                        VStack{
+                            Text("Invite friends on Flavor Feed")
+                            Text("flavor.feed/anonymous")
+                        }
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                })
+                UserListView()
+                NavigationView{
+                    VStack{
+                        //Text("\(selectedOption)")
+                        Picker("Tabs", selection: $selectedOption) {
+                            Text(options[0]).tag(options[0])
+                            Text(options[1]).tag(options[1])
+                            Text(options[2]).tag(options[2])
+                        }
+                        .pickerStyle(.segmented)
+                        .padding(.horizontal)
+                        .menuStyle(.borderlessButton)
+                    }
+                    .background(Color.ffTertiary)
+                }
+            }
+            .background(Color.ffTertiary)
         }
     }
     
--- FlavorFeed/Views/UserListView.swift
+++ FlavorFeed/Views/UserListView.swift
@@ -1,173 +1,64 @@
 import SwiftUI
 
 struct UserListView: View {
-    var user: User
-    let icon = "Image"
     var body: some View {
-        VStack{
-            HStack{
-                Image(icon)
-                    .resizable()
-                    .frame(width: 80, height: 70)
-                    .clipShape(Circle())
-                VStack(alignment: .leading) {
-                    Text(user.name).bold()
-                    Text(user.username)
+        var users: [User] = []
+
+        for _ in 1...30 {
+            let user = User(id: UUID(), name: "name1", username: "userName1", password: "", profilePicture: "", email: "", favorites: [], friends: [], savedPosts: [], bio: "", myPosts: [], phoneNumber: 0, location: "", myRecipes: [])
+            users.append(user)
+        }
+
+        return ScrollView {
+            VStack {
+                ForEach(users, id: \.id) { user in
+                    UserRow(user: user)
                 }
-                Spacer()
-                Button(action: {})
-                {
-                    Text("ADD")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                    .buttonStyle(.bordered)
-                    .background(Color(.darkGray))
-                    .cornerRadius(25)
-                Button(action: {})
-                {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 13, height: 13)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                    .padding([.trailing], 30)
-                    .padding([.leading], 10)
-            }
-            HStack{
-                Image(icon)
-                    .resizable()
-                    .frame(width: 80, height: 70)
-                    .clipShape(Circle())
-                VStack(alignment: .leading) {
-                    Text(user.name).bold()
-                    Text(user.username)
-                }
-                Spacer()
-                Button(action: {})
-                {
-                    Text("ADD")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                    .buttonStyle(.bordered)
-                    .background(Color(.darkGray))
-                    .cornerRadius(25)
-                Button(action: {})
-                {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 13, height: 13)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                    .padding([.trailing], 30)
-                    .padding([.leading], 10)
-            }
-            HStack{
-                Image(icon)
-                    .resizable()
-                    .frame(width: 80, height: 70)
-                    .clipShape(Circle())
-                VStack(alignment: .leading) {
-                    Text(user.name).bold()
-                    Text(user.username)
-                }
-                Spacer()
-                Button(action: {})
-                {
-                    Text("ADD")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                    .buttonStyle(.bordered)
-                    .background(Color(.darkGray))
-                    .cornerRadius(25)
-                Button(action: {})
-                {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 13, height: 13)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                    .padding([.trailing], 30)
-                    .padding([.leading], 10)
-            }
-            HStack{
-                Image(icon)
-                    .resizable()
-                    .frame(width: 80, height: 70)
-                    .clipShape(Circle())
-                VStack(alignment: .leading) {
-                    Text(user.name).bold()
-                    Text(user.username)
-                }
-                Spacer()
-                Button(action: {})
-                {
-                    Text("ADD")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                    .buttonStyle(.bordered)
-                    .background(Color(.darkGray))
-                    .cornerRadius(25)
-                Button(action: {})
-                {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 13, height: 13)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                    .padding([.trailing], 30)
-                    .padding([.leading], 10)
-            }
-            HStack{
-                Image(icon)
-                    .resizable()
-                    .frame(width: 80, height: 70)
-                    .clipShape(Circle())
-                VStack(alignment: .leading) {
-                    Text(user.name).bold()
-                    Text(user.username)
-                }
-                Spacer()
-                Button(action: {})
-                {
-                    Text("ADD")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                    .buttonStyle(.bordered)
-                    .background(Color(.darkGray))
-                    .cornerRadius(25)
-                Button(action: {})
-                {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 13, height: 13)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                    .padding([.trailing], 30)
-                    .padding([.leading], 10)
             }
         }
-        //Text(/@START_MENU_TOKEN@/"Hello, World!"/@END_MENU_TOKEN@/)
+        .frame(height: 450)
+    }
+
+    struct UserRow: View {
+        let icon = "Image"
+        let user: User
+
+        var body: some View {
+            HStack {
+                Image(icon)
+                    .resizable()
+                    .frame(width: 80, height: 70)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text(user.name).bold()
+                    Text(user.username)
+                }
+                Spacer()
+                Button(action: {}) {
+                    Text("ADD")
+                        .font(.system(size: 16))
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(.bordered)
+                .background(Color(.darkGray))
+                .cornerRadius(25)
+                Button(action: {}) {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 13, height: 13)
+                        .foregroundColor(.gray)
+                        .bold()
+                }
+                .padding([.trailing], 30)
+                .padding([.leading], 10)
+            }
+        }
     }
 }
 
 struct UserList_Previews: PreviewProvider {
     static var previews: some View {
-        let user = User(id: UUID(), name: "Triem", username: "triem123", password: "", profilePicture: "", email: "", favorites: [], friends: [], savedPosts: [], bio: "", myPosts: [], phoneNumber: 0, location: "", myRecipes: [])
-        UserListView(user: user)
+        UserListView()
     }
 }
