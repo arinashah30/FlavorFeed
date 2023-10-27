7a2c4f702bc1c9c319b69995bbd5df0c263dcd8e
arinashah30 <ashah634@gatech.edu>
2023-10-27 12:48:51 -0400

    Fixing missing files issue and empty posts issue

--- FlavorFeed/Views/SelfProfileView.swift
+++ FlavorFeed/Views/SelfProfileView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SelfProfileView: View {
     @Binding var tabSelection: Tabs
-    var user = User(id: UUID(), name: "Austin Huguenard", username: "austinhuguenard", password: "doesntmatter", profilePicture: "profile picture", email: "na", favorites: [Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [:], location: "none"), Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [:], location: "none"), Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [:], location: "none")], friends: [], savedPosts: [], bio: "nothing", myPosts: [], phoneNumber: 7705958485, location: "Georgia", myRecipes: [])
+    var user = User(id: "Austin", name: "Austin Huguenard", username: "austinhuguenard", profilePicture: "profile picture", email: "na", favorites: [Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [], location: "none"), Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [], location: "none"), Post(id: UUID(), images: ["waffles"], caption: "nothing", date: "nothing", likes: [], comments: [], location: "none")], friends: [], savedPosts: [], bio: "nothing", myPosts: [Post(id: UUID(), images: ["waffles"], caption: "Caption", date: "October 27th", likes: [], comments: [])], phoneNumber: 7705958485, location: "Georgia", myRecipes: [])
     var body: some View {
         VStack {
             HStack {
