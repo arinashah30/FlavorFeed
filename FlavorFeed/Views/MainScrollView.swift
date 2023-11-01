12f3432ba7f0b0600e3805641ac1a85786565666
arinashah30 <ashah634@gatech.edu>
2023-10-31 23:51:57 -0400

    Added hard coded posts to MainScrollView and fixed appearance by using List

--- FlavorFeed/Views/MainScrollView.swift
+++ FlavorFeed/Views/MainScrollView.swift
@@ -13,30 +13,41 @@ struct MainScrollView: View {
     var posts : [Post] = addPosts() //replace with vm.posts later
     
     var body: some View {
-        ZStack {
-            VStack {
-                TopBar(tabSelection: $tabSelection)
+        GeometryReader { geometry in
+            ZStack {
                 VStack {
-                    PostView(post: posts[0]).frame(maxHeight: .infinity)
-                }
-//                ScrollView {
-//                    ForEach(posts) {post in
-//                        PostView(post: post).frame(maxHeight: .infinity)
-//                    }
-//                }.frame(maxHeight: .infinity)
-            }.edgesIgnoringSafeArea(.bottom)
-            VStack {
-                Spacer()
-                BottomBar(messagesRemaing: Binding.constant(2)).frame(height: 100).edgesIgnoringSafeArea(.bottom)
-            }.edgesIgnoringSafeArea(.bottom).frame(maxHeight: .infinity)
-
+                    TopBar(tabSelection: $tabSelection)
+                    //                ScrollView {
+                    //                    VStack {
+                    //                        ForEach(posts) {post in
+                    //                            PostView(post: post).frame(maxHeight: .infinity)
+                    //                        }
+                    //                    }
+                    //                }.frame(maxHeight: .infinity)
+                    List(posts, id: \.id) { post in
+                        PostView(post: post).frame(width: geometry.size.width, height: 700)
+                    }.listStyle(.plain).listRowSeparatorTint(Color.clear)
+                }.edgesIgnoringSafeArea(.bottom)
+                VStack {
+                    Spacer()
+                    BottomBar(messagesRemaing: Binding.constant(2)).frame(height: 100).edgesIgnoringSafeArea(.bottom)
+                }.edgesIgnoringSafeArea(.bottom).frame(maxHeight: .infinity)
+                
+            }
         }
     }
 }
 
 //can remove this function when we get posts from firebase
 func addPosts() -> [Post] {
-    let posts: [Post] = [Post(id: UUID(), userID: UUID(), images: [["drake_selfie", "food_pic_1"], ["drake_selfie2", "food_pic_2"], ["drake_selfie3", "food_pic_3"]], caption: ["Let's dig in!", "", "That was yummy in my tummy"], date: "October 24, 2022", likes: [], comments: [Comment(id: UUID(), username: "Adonis", profilePicture: "adonis_pfp", text: "Looking fresh Drake!", date: "October 24, 2022", likes: [], replies: []), Comment(id: UUID(), username: "Travis Scott", profilePicture: "travis_scott_pfp", text: "She said do you love me I told her only partly.", date: "October 24, 2022", likes: [], replies: [])]), Post(id: UUID(), userID: UUID(), images: [["drake_selfie", "food_pic_1"], ["drake_selfie2", "food_pic_2"], ["drake_selfie3", "food_pic_3"]], caption: ["Let's dig in!", "", "That was yummy in my tummy"], date: "October 24, 2022", likes: [], comments: [Comment(id: UUID(), username: "Adonis", profilePicture: "adonis_pfp", text: "Looking fresh Drake!", date: "October 24, 2022", likes: [], replies: []), Comment(id: UUID(), username: "Travis Scott", profilePicture: "travis_scott_pfp", text: "She said do you love me I told her only partly.", date: "October 24, 2022", likes: [], replies: [])]), Post(id: UUID(), userID: UUID(), images: [["drake_selfie", "food_pic_1"], ["drake_selfie2", "food_pic_2"], ["drake_selfie3", "food_pic_3"]], caption: ["Let's dig in!", "", "That was yummy in my tummy"], date: "October 24, 2022", likes: [], comments: [Comment(id: UUID(), username: "Adonis", profilePicture: "adonis_pfp", text: "Looking fresh Drake!", date: "October 24, 2022", likes: [], replies: []), Comment(id: UUID(), username: "Travis Scott", profilePicture: "travis_scott_pfp", text: "She said do you love me I told her only partly.", date: "October 24, 2022", likes: [], replies: [])])]
+    var posts: [Post] = []
+    
+    for i in 1...30 {
+        
+        posts.append(Post(id: UUID(), userID: UUID(), images: [["drake_selfie", "food_pic_1"], ["drake_selfie2", "food_pic_2"], ["drake_selfie3", "food_pic_3"]], caption: ["That was yummy in my tummy", "", "Let's dig in"], date: "October 24, 2022", likes: [], comments: [Comment(id: UUID(), username: "Adonis", profilePicture: "adonis_pfp", text: "Looking fresh Drake!", date: "October 24, 2022", likes: [], replies: []), Comment(id: UUID(), username: "Travis Scott", profilePicture: "travis_scott_pfp", text: "She said do you love me I told her only partly.", date: "October 24, 2022", likes: [], replies: [])]))
+        
+        
+    }
     return posts
 }
 
