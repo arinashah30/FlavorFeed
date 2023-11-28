e90b7591b746cdd7d9df3a41e957e7fa8bb9e6a4
royankit11 <royankit11@gmail.com>
2023-11-27 21:15:24 -0500

    Mostly working version of Calendar feature

--- FlavorFeed/Views/CalendarItemView.swift
+++ FlavorFeed/Views/CalendarItemView.swift
@@ -11,12 +11,19 @@ struct CalendarItemView: View {
     var url: String
     var date: Date
     var postID: String
-    //var vm: ViewModel
+    var vm: ViewModel
     
-    init(date: Date, url: String, postID: String) {
+    init(date: Date, url: String, postID: String, vm: ViewModel) {
         self.date = date
         self.url = url
         self.postID = postID
+        self.vm = vm
+        
+        for num in 0..<vm.allMyPostDate.count {
+            if(Calendar.current.isDate(vm.allMyPostDate[num], equalTo: date, toGranularity: .day)) {
+                self.url = vm.allMyPostUrl[num]
+            }
+        }
     }
     
     var body: some View {
@@ -25,9 +32,10 @@ struct CalendarItemView: View {
             AsyncImage(
                 url: URL(string: url) ?? URL(string: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEWpqammpqbT09PX19fc3Nzi4uLm5uajo6Py8vLQ0NDd3d3+/v76+vrx8fHt7e3k5OQpkTr2AAABZ0lEQVR4nO3dQWrCABRF0URNorbV/e+2UmihOKgURE5yzwrenX/4w9v78eNyuczzfJ1uDt/2v+z+b/+nwyPO0wOu853jcF1+jL7lzmmYlmHNxt0wja8e8VSbKDyvvvBQoa1CX4W+Cn0V+ir03Qr3Fdoq9FXoq9BXoa9CX4W+Cn0V+ir0Veir0Fehr0Jfhb4KfZso3FVoq9BXoa9CX4W+Cn0V+ir0Veir0Fehr0Jfhb4KfRX6KvRV6KvQV6GvQt8mLoYqxFXoq9BXoa9CX4W+Cn0V+ir0Veir0Fehr0Jfhb4KfRX6KvRV6KvQV6FvE19YKsRV6KvQV6GvQl+Fvgp9Ffoq9FXoq9BXoa9CX4W+Cn0V+ir0baLwvPrCqUJbhb4KfRX6KvRV6KvQV6GvQl+Fvgp9Ffoq9FXoq9BXoa9CX4W+TRTOy6tHPFWFvgp9Ffoq9FXoq9BXoa9C31fhuGbLaTiedmt2mj8BNIYPDiZn2y8AAAAASUVORK5CYII="),
                 content: { image in
-                    image.resizable()
+                    image
+                        .resizable()
                         .aspectRatio(contentMode: .fill)
-                         .frame(maxWidth: 200, maxHeight: 300)
+                        .frame(maxHeight: 200)
                 },
                 placeholder: {
                     ProgressView()
@@ -50,9 +58,6 @@ struct CalendarItemView: View {
         }
     }
     
-    func getNDaysFromNow(days: Int) -> Date {
-        return Date(timeIntervalSinceNow: TimeInterval(days*24*3600))
-    }
     
     func getDayOfWeek(date: Date) -> String {
         let dateFormatter = DateFormatter()
@@ -69,5 +74,5 @@ struct CalendarItemView: View {
 }
 
 #Preview {
-    CalendarItemView(date: Date(), url: "", postID: "")
+    CalendarItemView(date: Date(), url: "", postID: "", vm: ViewModel())
 }
--- FlavorFeed/Views/CalendarView.swift
+++ FlavorFeed/Views/CalendarView.swift
@@ -11,49 +11,52 @@ struct CalendarView: View {
     
     @ObservedObject var vm: ViewModel
     @State private var calendarItemViews: [CalendarItemView] = []
-    let columns = [GridItem(.flexible(), spacing: 1), GridItem(.flexible(), spacing: 1), GridItem(.flexible(), spacing: 1)]
+    var dates: [Date] = []
+    let columns = [GridItem(.flexible(), spacing: 3), GridItem(.flexible(), spacing: 3), GridItem(.flexible(), spacing: 3)]
+    var count: Int = 0
 
     init(vm: ViewModel) {
         self.vm = vm
         getAllPosts(userID: "arinashah")
-        //loadCalendarItemViews()
+        
+        for num in -19...0 {
+            self.dates.append(getNDaysFromNow(days: num))
+        }
+        
+        print(self.dates)
     }
     
     var body: some View {
         
-        
-        LazyVGrid(columns: columns, spacing: 1) {
+        /*LazyVGrid(columns: columns, spacing: 1) {
             ForEach((0..<vm.allMyPostDate.count), id:\.self) { index in
                 
                 CalendarItemView(date: vm.allMyPostDate[index], url: vm.allMyPostUrl[index], postID: "")
                 
             }
             
-        }
+        }*/
+//        LazyVGrid(columns: columns, spacing: 1) {
+//            ForEach((0..<vm.allMyPostDate.count), id:\.self) { index in
+//                
+//                CalendarItemView(date: vm.allMyPostDate[index], url: vm.allMyPostUrl[index], postID: "")
+//                
+//            }
+//            
+//        }
         
-        }
-        
-    
-    func loadCalendarItemViews() {
-            for num in vm.allMyPosts {
-                print(vm.allMyPosts)
-                vm.get_day_and_url(postID: num) { day, url in
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "MM-dd-yyyy"
-                    print(url)
-                    var date = dateFormatter.date(from: day)
-                    let calendarItemView = CalendarItemView(date: date ?? Date(), url: url, postID: "")
-                    
-                    vm.calendarViews.append(calendarItemView)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 1) {
+                ForEach(dates, id:\.self) { index in
+                    CalendarItemView(date: index, url: "", postID: "", vm: vm)
                 }
             }
         }
+    }
     
     func getAllPosts(userID: String) {
         vm.getAllPostsOfUser(userID: userID) { postIDs in
             vm.allMyPosts = postIDs
-            
-            print(vm.allMyPosts)
             
             for post in vm.allMyPosts {
                 getPostInfo(postID: post)
@@ -71,6 +74,10 @@ struct CalendarView: View {
             vm.allMyPostUrl.append(url)
 
         }
+    }
+    
+    func getNDaysFromNow(days: Int) -> Date {
+        return Date(timeIntervalSinceNow: TimeInterval(days*24*3600))
     }
 }
 
