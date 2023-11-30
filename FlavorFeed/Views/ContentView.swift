5f06560968e3f33853d138a96a14767a7923badd
arinashah30 <ashah634@gatech.edu>
2023-11-30 11:16:42 -0500

    fixed variable for loginview

--- FlavorFeed/Views/ContentView.swift
+++ FlavorFeed/Views/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
         } else {
             NavigationStack {
                 LoginView(vm: vm).onAppear {
-                    isActive = true
+                    isActive = false
                 }
             }
         }
